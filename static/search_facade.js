4601cb935e14100661ac948f137175522993ce85
Jonathan Bowman <git@bowmanjd.com>
2023-06-26 08:56:47 -0400

    feat: better ko-fi icon, removed data-lang tab, created and updated

--- static/search_facade.js
+++ static/search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='f1e8a90abdec76f95a67a8503461a004be4128881d4f139e504cd798993689fb';
-        var sha384='Lv9vcpRgSafsYMjAe6Va/pbwLjO8s9KzlCXfN36Cc7h/Pd3+Mv4eH2qa4oKClpEd';
+        var sha256='9f6534fee00dee7b272e9bb2eeef14e5c314db17ec059085a674538b3e7933e4';
+        var sha384='9oaVLFrZnw5Jmxme60RjmBnD/MZYEUunmuljDgeLpGQqeQzoyArALLXDOaGWAE0s';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
