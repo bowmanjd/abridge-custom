97aa840e321be7b1189a5636b6401b79ba71d3d3
Jonathan Bowman <git@bowmanjd.com>
2023-06-29 06:54:07 -0400

    new hashes

--- static/search_facade.js
+++ static/search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='9f6534fee00dee7b272e9bb2eeef14e5c314db17ec059085a674538b3e7933e4';
-        var sha384='9oaVLFrZnw5Jmxme60RjmBnD/MZYEUunmuljDgeLpGQqeQzoyArALLXDOaGWAE0s';
+        var sha256='5f30dbdc5a7dd5f0679ff7d1e54ba0af5fd0b7676b8b2c383fd88a21d2aa1477';
+        var sha384='dxaQs8Jn0988WfgCKBOpbFILnNf014QWObgFNmuWF4Hjz5uWIlGkHBZxndF3RqiE';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
