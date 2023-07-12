a61663049ce40a8b17a007ec657400c1f991e5f0
Jonathan Bowman <git@bowmanjd.com>
2023-07-12 07:04:18 -0400

    search update

--- static/search_facade.js
+++ static/search_facade.js
@@ -4,8 +4,8 @@ window.onload = function() {
         if (baseUrl.slice(-1) == "/") {
             baseUrl = baseUrl.slice(0, -1);
         }
-        var sha256='564eda9a33ba33dc73b95ef8f22a35fc127c4b2733212d52c62e7623b6a9888c';
-        var sha384='y/j279OkgkW4AVNLF5iK3e4qfEtnwQ8qSQzgHkq/3EI5RYqM52jyDpfoqDirL4mO';
+        var sha256='a9575107daaaa12728d2f6cf5b935f3ff7cb2768fb526684b73c4d7bc47f2808';
+        var sha384='noTO+O8fQkdwUYl5SKV+MD3BJIAscyOv1d+no+yn/5KGTkzXNTBS6ZNP154V8TD0';
         var loadSearch = document.createElement('script');
         loadSearch.src = baseUrl + '/search_bundle.min.js?h=' + sha256;
         loadSearch.setAttribute('integrity', 'sha384-' + sha384);
